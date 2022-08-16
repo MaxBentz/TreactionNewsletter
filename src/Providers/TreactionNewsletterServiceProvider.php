40325ba393671ce6ac54f768b9ddec20762ae7dc
cerwantes <max1162@web.de>
2022-08-16 11:00:56 +0200

    added js implementation

--- src/Providers/TreactionNewsletterServiceProvider.php
+++ src/Providers/TreactionNewsletterServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace TreactionNewsletter\Providers;
 
+use IO\Helper\ResourceContainer;
 use Plenty\Plugin\ConfigRepository;
 use Plenty\Modules\Webshop\Template\Providers\TemplateServiceProvider;
 use Plenty\Plugin\Events\Dispatcher;
@@ -15,6 +16,10 @@ class TreactionNewsletterServiceProvider extends TemplateServiceProvider
 
     public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
     {
+        $eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container) {
+            $container->addScriptTemplate('TreactionNewsletter::Javascript');
+        }, 0);
+
         $this->overrideTemplate("Ceres::Widgets.Common.NewsletterWidget", "TreactionNewsletter::Widgets.Common.NewsletterWidget");
     }
 }
