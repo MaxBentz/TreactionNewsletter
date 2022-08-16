7434f9e4d1495221d2cc7c5bed1e7cc260a43b72
cerwantes <max1162@web.de>
2022-08-16 10:10:39 +0200

    test

--- src/Providers/TreactionNewsletterServiceProvider.php
+++ src/Providers/TreactionNewsletterServiceProvider.php
@@ -3,17 +3,14 @@
 namespace TreactionNewsletter\Providers;
 
 use Plenty\Plugin\ConfigRepository;
-use TreactionNewsletter\Components\PluginConfig;
 use Plenty\Modules\Webshop\Template\Providers\TemplateServiceProvider;
 use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 
-class TreactionNewsletterServiceProvider extends ServiceProvider
+class TreactionNewsletterServiceProvider extends TemplateServiceProvider
 {
 
     public function register() {
-        $this->getApplication()->singleton(PluginConfig::class);
     }
 
     public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
